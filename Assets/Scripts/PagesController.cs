4c268464c82b7e3ae468f20ae285cb70ed0c6e87
PePo <gmgame.1525@gmail.com>
2019-04-23 00:42:20 +0700

    Add Lean touch

---
+++ Assets/Scripts/PagesController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PagesController : MonoBehaviour
+{
+
+	[SerializeField] private Canvas[] pagesCanvas;
+	[SerializeField] private GameObject statusGroup;
+
+	private void Start()
+	{
+		
+	}
+
+	private void Update()
+	{
+
+	}
+}
