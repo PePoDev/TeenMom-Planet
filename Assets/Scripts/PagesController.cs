db36ffab5f941ccb9b543845fb12db4f8504e02c
PePo <gmgame.1525@gmail.com>
2019-04-23 05:53:41 +0700

    Detailed in home page layout

--- Assets/Scripts/PagesController.cs
+++ Assets/Scripts/PagesController.cs
@@ -5,17 +5,45 @@ using UnityEngine.UI;
 
 public class PagesController : MonoBehaviour
 {
+	public Image[] statusPageActive;
+	public Sprite spriteEnablePageActive;
+	public Sprite spritedisablePageActive;
 
-	[SerializeField] private Canvas[] pagesCanvas;
-	[SerializeField] private GameObject statusGroup;
+	private Animator animator;
+	private int pageIdx;
 
 	private void Start()
 	{
-		
+		pageIdx = 0;
+		animator = GetComponent<Animator>();
 	}
 
-	private void Update()
+	public void SwitchPageRight()
 	{
+		if (pageIdx + 1 == statusPageActive.Length)
+			return;
 
+		SwitchPage(pageIdx++);
+	}
+
+	public void SwitchPageLeft()
+	{
+		if (pageIdx < 0)
+			return;
+
+		SwitchPage(pageIdx--);
+	}
+
+	public void SwitchPage(int pageIdx)
+	{
+		if (this.pageIdx != pageIdx)
+		{
+			animator.SetInteger("pageIdx", pageIdx);
+			this.pageIdx = pageIdx;
+			for (int i = 0; i < statusPageActive.Length; i++)
+			{
+				statusPageActive[i].sprite = pageIdx == i ? spriteEnablePageActive : spritedisablePageActive;
+			}
+		}
 	}
 }
---
+++ Assets/Scripts/Utils.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Utils : MonoBehaviour
+{
+	
+	public void LoadNextScene()
+	{
+		Initiate.Fade(SceneManager.GetActiveScene().buildIndex + 1, Color.black, 1f);
+	}
+
+	public void OpenUrl(string url)
+	{
+		Application.OpenURL(url);
+	}
+}
