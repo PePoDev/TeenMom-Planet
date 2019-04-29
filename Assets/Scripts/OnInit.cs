d4bc7a5536206b5d2e17937b37e10d95f2722129
PePo <gmgame.1525@gmail.com>
2019-04-29 22:58:43 +0700

    [Complete] Add video ending in last scene

---
+++ Assets/Scripts/OnInit.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnInit : MonoBehaviour
+{
+	public UnityEvent OnAwake;
+	public UnityEvent OnStart;
+
+	private void Awake()
+	{
+		OnAwake.Invoke();
+	}
+
+	private void Start()
+	{
+		OnStart.Invoke();
+	}
+}
